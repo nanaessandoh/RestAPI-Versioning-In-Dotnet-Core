63f124c43da42a46e466004e72759f73c320e669
Nana Essandoh <nanaessandoh@gmail.com>
2021-06-02 07:19:34 -0400

    Modified Controller for Presentation

--- APIVersioning.Web/Controllers/WeatherForecastController.cs
+++ APIVersioning.Web/Controllers/WeatherForecastController.cs
@@ -1,38 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace APIVersioning.Web.Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/weather")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        public WeatherForecastController(){ }
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [HttpGet("{city}")]
+        public IActionResult Get([FromRoute] string city)
         {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var weatherInfo = new WeatherInfo
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Name = city,
+                CurrentTime = DateTime.UtcNow,
+                Temperature = 2,
+                Precipitation = 0.2,
+                Humidity = 0.77,
+                //Gist = "Cloudy with some rainfall in the evening."
+
+            };
+
+            return Ok(weatherInfo);
         }
+    }
+
+    internal class WeatherInfo
+    {
+        public string Name { get; set; }
+        public DateTime CurrentTime { get; set; }
+        public int Temperature { get; set; }
+        public double Precipitation { get; set; }
+        public double Humidity { get; set; }
+        public string Gist { get; set; }
     }
 }
